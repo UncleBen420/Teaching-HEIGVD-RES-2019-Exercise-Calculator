05baa462dd7b0a64bca2c7d2c460adf9ea672b6d
UncleBen420 <remyvu@outlook.com>
2019-03-28 10:15:36 +0100

    add something

--- specs/UncleBen420/Calculatrice_Server/src/test/java/Client/ClientTest.java
+++ specs/UncleBen420/Calculatrice_Server/src/test/java/Client/ClientTest.java
@@ -1,7 +1,17 @@
 package Client;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class ClientTest extends TestCase {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	
+	
 
 }
--- specs/UncleBen420/Calculatrice_Server/src/test/java/Server/ServerTest.java
+++ specs/UncleBen420/Calculatrice_Server/src/test/java/Server/ServerTest.java
@@ -1,7 +1,13 @@
 package Server;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class ServerTest extends TestCase {
-
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
 }
