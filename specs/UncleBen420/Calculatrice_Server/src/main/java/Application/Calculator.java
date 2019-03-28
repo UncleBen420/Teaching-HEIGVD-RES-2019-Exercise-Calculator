a11db43a10437cc2a93f0f5252f90998518db24f
Dherleth <erwan.moreira@gmail.com>
2019-03-28 10:24:03 +0100

    Ajouté un if dans l'app

--- specs/UncleBen420/Calculatrice_Server/src/main/java/Application/Calculator.java
+++ specs/UncleBen420/Calculatrice_Server/src/main/java/Application/Calculator.java
@@ -4,23 +4,28 @@ import Client.Client;
 
 public class Calculator {
     public static void main(String[] args) {
-        if(args[0].equals("-s")) {
-
-        } else if(args[0].equals("-c")) {
-
-            if(args.length < 4) {
-                System.out.println("Need : serverIP serverPort Calculus");
-            } else {
-                String serverIP = args[1];
-                int serverPort = Integer.parseInt(args[2]);
-                String calculus = args[3];
-
-                Client client = new Client();
-                client.sendCalculus(serverIP, serverPort, calculus);
-            }
+        if (args.length == 0) {
+            System.out.println("Missing arguments : \n" +
+                    "-s to lauch the server, -c to launch the client)");
         } else {
-            System.out.println("Wrong parameter !\n" +
-                               "-s to lauch the server, -c to launch the client)");
+            if (args[0].equals("-s")) {
+
+            } else if (args[0].equals("-c")) {
+
+                if (args.length < 4) {
+                    System.out.println("Need : serverIP serverPort Calculus");
+                } else {
+                    String serverIP = args[1];
+                    int serverPort = Integer.parseInt(args[2]);
+                    String calculus = args[3];
+
+                    Client client = new Client();
+                    client.sendCalculus(serverIP, serverPort, calculus);
+                }
+            } else {
+                System.out.println("Wrong parameter : \n" +
+                        "-s to lauch the server, -c to launch the client)");
+            }
         }
     }
 }
